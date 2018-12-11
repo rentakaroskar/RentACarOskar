d2f9508c2cf8502be2ab209bb8fc71f3261aeb80
david.ruzicic <david.ruzicic@NUC1-17-PC>
2018-12-11 11:10:32 +0100

    Dodao atribute ForeignKey, PrimaryKey, SqlName

---
+++ RentACarOskar/RentACarOskar/Attributes/ForeignKeyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarOskar.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class ForeignKeyAttribute : Attribute
+    {
+        string TableName;
+        string ColumnName;
+
+        public ForeignKeyAttribute(string tableName, string columnName)
+        {
+            TableName = tableName;
+            ColumnName = columnName;
+        }
+    }
+}
---
+++ RentACarOskar/RentACarOskar/Attributes/SqlNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarOskar.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class SqlNameAttribute : Attribute
+    {
+        string Name;
+
+        public SqlNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
