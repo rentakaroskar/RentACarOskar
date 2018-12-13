78a12ed35319d8170ccbf26b39aa12689b852fe6
sanja.mm <student@itivity.com>
2018-12-13 12:19:50 +0100

    Prepravka

--- RentACarOskar/RentACarOskar/Dashboard.cs
+++ RentACarOskar/RentACarOskar/Dashboard.cs
@@ -1,22 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using KonekcijaNaBazu;
+using RentACarOskar.Attributes;
+using RentACarOskar.PropertyClass;
+using System;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace RentACarOskar
 {
     public partial class Dashboard : Form
     {
+
         public Dashboard()
         {
             InitializeComponent();
+            PropertyVozilo pom = new PropertyVozilo();
+            PopulateGrid(pom);
         }
 
+        private void PopulateGrid(PropertyInterface myProperty)
+        {
+            panelPanelZaGV.Controls.Clear();
+            DataTable dt = new DataTable();
+            DataGridView dgv = new DataGridView();
+            panelPanelZaGV.Controls.Add(dgv);
+            dgv.Size = panelPanelZaGV.Size;
+            //logika za popunjavanje tabele
+            SqlDataReader reader = SqlHelper.ExecuteReader(SqlHelper.GetConnectionString(), CommandType.Text,
+                myProperty.GetSelectQuery());
+
+            dt.Load(reader);
+            reader.Close();
+
+            dgv.DataSource = dt; //prikazi tabelu
+
+            //izvuci display name
+            var type = myProperty.GetType();
+            var properties = type.GetProperties();
+
+            //promijeniti nazive kolona
+            foreach (DataGridViewColumn item in dgv.Columns)
+            {
+                item.HeaderText = properties.Where(x => x.GetCustomAttributes<SqlNameAttribute>().FirstOrDefault().Name == item.HeaderText
+                ).FirstOrDefault().GetCustomAttributes<DisplayNameAttribute>().FirstOrDefault().DisplayName;
+            }
+        }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             if (PanelLeft.Width == 245)
@@ -25,23 +56,23 @@ namespace RentACarOskar
                 logoPic.Visible = false;
             }
             else
+            {
                 PanelLeft.Width = 245;
                 logoPic.Visible = true;
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox3_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
+            }
         }
 
         private void btnVozilo_Click(object sender, EventArgs e)
         {
-
+            PropertyVozilo pom = new PropertyVozilo();
+            PopulateGrid(pom);
         }
+
+        private void btnRadnik_Click(object sender, EventArgs e)
+        {
+            PropertyRadnik pom = new PropertyRadnik();
+            PopulateGrid(pom);
+        }
+
     }
 }
