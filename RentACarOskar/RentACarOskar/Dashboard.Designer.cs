9ee748afa7aa597bb8f8cd84b478fa3270ce5fc9
katarinagrcic <katarinagrcic@outlook.com>
2018-12-17 16:54:31 +0100

    Rjeseno

--- RentACarOskar/RentACarOskar/Dashboard.Designer.cs
+++ RentACarOskar/RentACarOskar/Dashboard.Designer.cs
@@ -35,9 +35,6 @@
             this.Dobrodosli = new System.Windows.Forms.Label();
             this.panelFilter = new System.Windows.Forms.Panel();
             this.btnFilter = new MetroFramework.Controls.MetroButton();
-            this.btnDelete = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.btnUpdate = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.btnInsert = new Bunifu.Framework.UI.BunifuFlatButton();
             this.panelPanelZaGV = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.PanelTop = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -113,7 +110,7 @@
             this.panelCentar.Controls.Add(this.Dobrodosli);
             this.panelCentar.Controls.Add(this.panelFilter);
             this.panelCentar.Controls.Add(this.btnFilter);
-            this.panelCentar.Controls.Add(this.btnDelete);
+            this.panelCentar.Controls.Add(this.bDelete);
             this.panelCentar.Controls.Add(this.btnUpdate);
             this.panelCentar.Controls.Add(this.btnInsert);
             this.panelCentar.Controls.Add(this.Dobrodosli);
@@ -157,109 +154,30 @@
             this.btnFilter.UseSelectable = true;
             this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
             // 
-            // btnDelete
-            // 
-            this.btnDelete.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
-            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
-            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnDelete.BorderRadius = 6;
-            this.btnDelete.ButtonText = "DELETE";
-            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDelete.DisabledColor = System.Drawing.Color.Gray;
-            this.btnDelete.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnDelete.Iconimage = null;
-            this.btnDelete.Iconimage_right = null;
-            this.btnDelete.Iconimage_right_Selected = null;
-            this.btnDelete.Iconimage_Selected = null;
-            this.btnDelete.IconMarginLeft = 0;
-            this.btnDelete.IconMarginRight = 0;
-            this.btnDelete.IconRightVisible = true;
-            this.btnDelete.IconRightZoom = 0D;
-            this.btnDelete.IconVisible = true;
-            this.btnDelete.IconZoom = 90D;
-            this.btnDelete.IsTab = false;
-            this.btnDelete.Location = new System.Drawing.Point(717, 590);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
-            this.btnDelete.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
-            this.btnDelete.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnDelete.selected = false;
-            this.btnDelete.Size = new System.Drawing.Size(108, 34);
-            this.btnDelete.TabIndex = 3;
-            this.btnDelete.Text = "DELETE";
-            this.btnDelete.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnDelete.Textcolor = System.Drawing.Color.White;
-            this.btnDelete.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
             // btnUpdate
             // 
-            this.btnUpdate.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
             this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
             this.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnUpdate.BorderRadius = 6;
-            this.btnUpdate.ButtonText = "UPDATE";
             this.btnUpdate.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnUpdate.DisabledColor = System.Drawing.Color.Gray;
-            this.btnUpdate.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnUpdate.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnUpdate.Iconimage")));
-            this.btnUpdate.Iconimage_right = null;
-            this.btnUpdate.Iconimage_right_Selected = null;
-            this.btnUpdate.Iconimage_Selected = null;
-            this.btnUpdate.IconMarginLeft = 0;
-            this.btnUpdate.IconMarginRight = 0;
-            this.btnUpdate.IconRightVisible = true;
-            this.btnUpdate.IconRightZoom = 0D;
-            this.btnUpdate.IconVisible = true;
-            this.btnUpdate.IconZoom = 90D;
-            this.btnUpdate.IsTab = false;
             this.btnUpdate.Location = new System.Drawing.Point(396, 590);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
-            this.btnUpdate.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
-            this.btnUpdate.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnUpdate.selected = false;
             this.btnUpdate.Size = new System.Drawing.Size(108, 34);
             this.btnUpdate.TabIndex = 2;
             this.btnUpdate.Text = "UPDATE";
             this.btnUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnUpdate.Textcolor = System.Drawing.Color.White;
-            this.btnUpdate.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnInsert
             // 
-            this.btnInsert.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
             this.btnInsert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
             this.btnInsert.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnInsert.BorderRadius = 6;
-            this.btnInsert.ButtonText = "INSERT";
             this.btnInsert.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnInsert.DisabledColor = System.Drawing.Color.Gray;
-            this.btnInsert.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnInsert.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnInsert.Iconimage")));
-            this.btnInsert.Iconimage_right = null;
-            this.btnInsert.Iconimage_right_Selected = null;
-            this.btnInsert.Iconimage_Selected = null;
-            this.btnInsert.IconMarginLeft = 0;
-            this.btnInsert.IconMarginRight = 0;
-            this.btnInsert.IconRightVisible = true;
-            this.btnInsert.IconRightZoom = 0D;
-            this.btnInsert.IconVisible = true;
-            this.btnInsert.IconZoom = 90D;
-            this.btnInsert.IsTab = false;
             this.btnInsert.Location = new System.Drawing.Point(132, 590);
             this.btnInsert.Name = "btnInsert";
-            this.btnInsert.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
-            this.btnInsert.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
-            this.btnInsert.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnInsert.selected = false;
             this.btnInsert.Size = new System.Drawing.Size(108, 34);
             this.btnInsert.TabIndex = 1;
             this.btnInsert.Text = "INSERT";
             this.btnInsert.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnInsert.Textcolor = System.Drawing.Color.White;
-            this.btnInsert.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
             // 
             // panelPanelZaGV
