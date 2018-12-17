d4bb243170032cfc0fb60834aa8d89dacf373ec1
david.ruzicic <david.ruzicic@NUC1-17-PC>
2018-12-17 13:06:42 +0100

    Dodan atribut radio button

---
+++ RentACarOskar/RentACarOskar/Attributes/TwoRadioButtonsAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarOskar.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class TwoRadioButtonsAttribute : Attribute
+    {
+        public string Value1;
+        public string Value2;
+
+        public TwoRadioButtonsAttribute(string value1, string value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+    }
+}
