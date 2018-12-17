2e5dc2c35146400c243a312a6d6a923712738277
lukaG96 <luka1996gacic@gmail.com>
2018-12-17 12:19:32 +0100

    dodao btn-e na lookup formu

--- RentACarOskar/RentACarOskar/LookUpForma.Designer.cs
+++ RentACarOskar/RentACarOskar/LookUpForma.Designer.cs
@@ -29,34 +29,66 @@
         private void InitializeComponent()
         {
             this.dgStandardGrid = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnOk = new MetroFramework.Controls.MetroButton();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgStandardGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // dgStandardGrid
             // 
             this.dgStandardGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgStandardGrid.Location = new System.Drawing.Point(43, 88);
+            this.dgStandardGrid.Location = new System.Drawing.Point(23, 86);
             this.dgStandardGrid.Name = "dgStandardGrid";
             this.dgStandardGrid.Size = new System.Drawing.Size(579, 303);
             this.dgStandardGrid.TabIndex = 0;
             // 
-            // button1
+            // btnOk
             // 
-            this.button1.Location = new System.Drawing.Point(505, 404);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnReturn_Click);
+            this.btnOk.Location = new System.Drawing.Point(23, 404);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "Add New";
+            this.btnOk.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(104, 404);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Update";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(185, 404);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.TabIndex = 4;
+            this.metroButton2.Text = "Delete";
+            this.metroButton2.UseSelectable = true;
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(527, 404);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(75, 23);
+            this.metroButton3.TabIndex = 5;
+            this.metroButton3.Text = "Return";
+            this.metroButton3.UseSelectable = true;
             // 
             // LookUpForma
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(636, 440);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.btnOk);
             this.Controls.Add(this.dgStandardGrid);
             this.Name = "LookUpForma";
             this.Text = "LookUpForma";
@@ -68,6 +100,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgStandardGrid;
-        private System.Windows.Forms.Button button1;
+        private MetroFramework.Controls.MetroButton btnOk;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
     }
 }
