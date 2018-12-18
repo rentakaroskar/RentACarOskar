6df76be0901be55e442b2c51d3498b3ec2fd25f4
sanja.mm <student@itivity.com>
2018-12-18 15:09:46 +0100

    Dodano izdavanje racuna

--- RentACarOskar/RentACarOskar/Dashboard.Designer.cs
+++ RentACarOskar/RentACarOskar/Dashboard.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.panelCentar = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.btnIzdaj = new Bunifu.Framework.UI.BunifuFlatButton();
             this.Dobrodosli = new System.Windows.Forms.Label();
             this.btnDelete = new Bunifu.Framework.UI.BunifuFlatButton();
             this.btnUpdate = new Bunifu.Framework.UI.BunifuFlatButton();
@@ -40,6 +41,8 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.PanelLeft = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.panelMenu = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblLogOut = new System.Windows.Forms.Label();
             this.panelFaktura = new System.Windows.Forms.Panel();
             this.lblFaktura = new System.Windows.Forms.Label();
             this.btnFaktura = new Bunifu.Framework.UI.BunifuImageButton();
@@ -51,15 +54,13 @@
             this.slicicaAuto = new Bunifu.Framework.UI.BunifuImageButton();
             this.loptica = new System.Windows.Forms.PictureBox();
             this.logoPic = new System.Windows.Forms.PictureBox();
-            this.btnIzdaj = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lblLogOut = new System.Windows.Forms.Label();
             this.panelCentar.SuspendLayout();
             this.PanelTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.PanelLeft.SuspendLayout();
             this.panelMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.panelFaktura.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnFaktura)).BeginInit();
             this.panelKlijenti.SuspendLayout();
@@ -68,7 +69,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.slicicaAuto)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.loptica)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.logoPic)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelCentar
@@ -91,6 +91,41 @@
             this.panelCentar.Quality = 10;
             this.panelCentar.Size = new System.Drawing.Size(927, 650);
             this.panelCentar.TabIndex = 2;
+            // 
+            // btnIzdaj
+            // 
+            this.btnIzdaj.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
+            this.btnIzdaj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
+            this.btnIzdaj.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnIzdaj.BorderRadius = 6;
+            this.btnIzdaj.ButtonText = "IZDAJ";
+            this.btnIzdaj.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIzdaj.DisabledColor = System.Drawing.Color.Gray;
+            this.btnIzdaj.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnIzdaj.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnIzdaj.Iconimage")));
+            this.btnIzdaj.Iconimage_right = null;
+            this.btnIzdaj.Iconimage_right_Selected = null;
+            this.btnIzdaj.Iconimage_Selected = null;
+            this.btnIzdaj.IconMarginLeft = 0;
+            this.btnIzdaj.IconMarginRight = 0;
+            this.btnIzdaj.IconRightVisible = true;
+            this.btnIzdaj.IconRightZoom = 0D;
+            this.btnIzdaj.IconVisible = true;
+            this.btnIzdaj.IconZoom = 90D;
+            this.btnIzdaj.IsTab = false;
+            this.btnIzdaj.Location = new System.Drawing.Point(557, 590);
+            this.btnIzdaj.Name = "btnIzdaj";
+            this.btnIzdaj.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
+            this.btnIzdaj.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
+            this.btnIzdaj.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnIzdaj.selected = false;
+            this.btnIzdaj.Size = new System.Drawing.Size(108, 34);
+            this.btnIzdaj.TabIndex = 3;
+            this.btnIzdaj.Text = "IZDAJ";
+            this.btnIzdaj.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIzdaj.Textcolor = System.Drawing.Color.White;
+            this.btnIzdaj.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIzdaj.Click += new System.EventHandler(this.btnIzdaj_Click);
             // 
             // Dobrodosli
             // 
@@ -295,6 +330,27 @@
             this.panelMenu.Size = new System.Drawing.Size(245, 700);
             this.panelMenu.TabIndex = 0;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(46)))), ((int)(((byte)(62)))));
+            this.panel1.Controls.Add(this.lblLogOut);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(0, 578);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 45);
+            this.panel1.TabIndex = 10;
+            // 
+            // lblLogOut
+            // 
+            this.lblLogOut.AutoSize = true;
+            this.lblLogOut.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogOut.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblLogOut.Location = new System.Drawing.Point(68, 13);
+            this.lblLogOut.Name = "lblLogOut";
+            this.lblLogOut.Size = new System.Drawing.Size(80, 22);
+            this.lblLogOut.TabIndex = 7;
+            this.lblLogOut.Text = "Log out";
+            // 
             // panelFaktura
             // 
             this.panelFaktura.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(46)))), ((int)(((byte)(62)))));
@@ -436,62 +492,6 @@
             this.logoPic.TabIndex = 0;
             this.logoPic.TabStop = false;
             // 
-            // btnIzdaj
-            // 
-            this.btnIzdaj.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
-            this.btnIzdaj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
-            this.btnIzdaj.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnIzdaj.BorderRadius = 6;
-            this.btnIzdaj.ButtonText = "IZDAJ";
-            this.btnIzdaj.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnIzdaj.DisabledColor = System.Drawing.Color.Gray;
-            this.btnIzdaj.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnIzdaj.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnIzdaj.Iconimage")));
-            this.btnIzdaj.Iconimage_right = null;
-            this.btnIzdaj.Iconimage_right_Selected = null;
-            this.btnIzdaj.Iconimage_Selected = null;
-            this.btnIzdaj.IconMarginLeft = 0;
-            this.btnIzdaj.IconMarginRight = 0;
-            this.btnIzdaj.IconRightVisible = true;
-            this.btnIzdaj.IconRightZoom = 0D;
-            this.btnIzdaj.IconVisible = true;
-            this.btnIzdaj.IconZoom = 90D;
-            this.btnIzdaj.IsTab = false;
-            this.btnIzdaj.Location = new System.Drawing.Point(557, 590);
-            this.btnIzdaj.Name = "btnIzdaj";
-            this.btnIzdaj.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
-            this.btnIzdaj.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
-            this.btnIzdaj.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnIzdaj.selected = false;
-            this.btnIzdaj.Size = new System.Drawing.Size(108, 34);
-            this.btnIzdaj.TabIndex = 3;
-            this.btnIzdaj.Text = "IZDAJ";
-            this.btnIzdaj.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnIzdaj.Textcolor = System.Drawing.Color.White;
-            this.btnIzdaj.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIzdaj.Click += new System.EventHandler(this.btnIzdaj_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(46)))), ((int)(((byte)(62)))));
-            this.panel1.Controls.Add(this.lblLogOut);
-            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.panel1.Location = new System.Drawing.Point(0, 578);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(245, 45);
-            this.panel1.TabIndex = 10;
-            // 
-            // lblLogOut
-            // 
-            this.lblLogOut.AutoSize = true;
-            this.lblLogOut.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogOut.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.lblLogOut.Location = new System.Drawing.Point(68, 13);
-            this.lblLogOut.Name = "lblLogOut";
-            this.lblLogOut.Size = new System.Drawing.Size(80, 22);
-            this.lblLogOut.TabIndex = 7;
-            this.lblLogOut.Text = "Log out";
-            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -512,6 +512,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.PanelLeft.ResumeLayout(false);
             this.panelMenu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.panelFaktura.ResumeLayout(false);
             this.panelFaktura.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnFaktura)).EndInit();
@@ -523,8 +525,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.slicicaAuto)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.loptica)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.logoPic)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
---
+++ RentACarOskar/RentACarOskar/FormaIzdavanje.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace RentACarOskar
+{
+    partial class FormaIzdavanje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStampaj = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.SuspendLayout();
+            // 
+            // btnStampaj
+            // 
+            this.btnStampaj.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
+            this.btnStampaj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
+            this.btnStampaj.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStampaj.BorderRadius = 6;
+            this.btnStampaj.ButtonText = "STAMPAJ";
+            this.btnStampaj.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStampaj.DisabledColor = System.Drawing.Color.Gray;
+            this.btnStampaj.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnStampaj.Iconimage = null;
+            this.btnStampaj.Iconimage_right = null;
+            this.btnStampaj.Iconimage_right_Selected = null;
+            this.btnStampaj.Iconimage_Selected = null;
+            this.btnStampaj.IconMarginLeft = 0;
+            this.btnStampaj.IconMarginRight = 0;
+            this.btnStampaj.IconRightVisible = true;
+            this.btnStampaj.IconRightZoom = 0D;
+            this.btnStampaj.IconVisible = true;
+            this.btnStampaj.IconZoom = 90D;
+            this.btnStampaj.IsTab = false;
+            this.btnStampaj.Location = new System.Drawing.Point(660, 393);
+            this.btnStampaj.Name = "btnStampaj";
+            this.btnStampaj.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(135)))), ((int)(((byte)(150)))));
+            this.btnStampaj.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(181)))), ((int)(((byte)(88)))));
+            this.btnStampaj.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnStampaj.selected = false;
+            this.btnStampaj.Size = new System.Drawing.Size(108, 34);
+            this.btnStampaj.TabIndex = 4;
+            this.btnStampaj.Text = "STAMPAJ";
+            this.btnStampaj.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnStampaj.Textcolor = System.Drawing.Color.White;
+            this.btnStampaj.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // FormaIzdavanje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStampaj);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
+            this.Name = "FormaIzdavanje";
+            this.Resizable = false;
+            this.Text = "FormaIzdavanje";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuFlatButton btnStampaj;
+    }
+}
