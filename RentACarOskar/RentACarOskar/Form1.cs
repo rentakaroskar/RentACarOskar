4ce811fae2a566cf7edfc7e2d8f61df08877d257
lukaG96 <luka1996gacic@gmail.com>
2018-12-20 09:17:26 +0100

    auto zatvaranje message boxa uspjesan login nakon 1s

--- RentACarOskar/RentACarOskar/Form1.cs
+++ RentACarOskar/RentACarOskar/Form1.cs
@@ -64,6 +64,7 @@ namespace RentACarOskar
                 }
             }
 
+
             //private void btnLogIn_Click(object sender, EventArgs e)
             //{
             //    if (tbUserName.Text == "" || tbPassword.Text == "")
@@ -138,7 +139,8 @@ namespace RentACarOskar
             {
                 if (dt.Rows[i][7].ToString() == tbPassword.Text && dt.Rows[i][8].ToString() == tbUserName.Text)
                 {
-                    MessageBox.Show("Uspjesan Login");
+                    //MessageBox.Show("Uspjesan Login");
+                    AutoClosingMessageBox.Show("Uspjesan Login", "LogIn", 1000);
                     Dashboard pom = new Dashboard(mail);
                     Visible = false;
                     pom.ShowDialog();
@@ -167,6 +169,40 @@ namespace RentACarOskar
         {
             btnLogIn.BackColor = Color.FromArgb(20, 255, 255, 255);
 
+        }
+
+
+
+        public class AutoClosingMessageBox
+        { 
+
+            System.Threading.Timer _timeoutTimer;
+            string _caption;
+            AutoClosingMessageBox(string text, string caption, int timeout)
+            {
+                _caption = caption;
+                _timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                    null, timeout, System.Threading.Timeout.Infinite);
+                using (_timeoutTimer)
+                    MessageBox.Show(text, caption);
+                 
+            }
+            public static void Show(string text, string caption, int timeout)
+            {
+                new AutoClosingMessageBox(text, caption, timeout);
+            }
+            void OnTimerElapsed(object state)
+            {
+                IntPtr mbWnd = FindWindow("#32770", _caption); // lpClassName is #32770 for MessageBox
+                if (mbWnd != IntPtr.Zero)
+                    SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                _timeoutTimer.Dispose();
+            }
+            const int WM_CLOSE = 0x0010;
+            [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+            static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+            static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
         }
     }
 }
