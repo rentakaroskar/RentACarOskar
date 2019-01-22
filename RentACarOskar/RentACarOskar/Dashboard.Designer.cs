732a46d1e2ac9bd94d713bf9137ac7afe1571147
david.ruzicic <david.ruzicic@NUC1-17-PC>
2019-01-22 14:10:01 +0100

    Pokusaj

--- RentACarOskar/RentACarOskar/Dashboard.Designer.cs
+++ RentACarOskar/RentACarOskar/Dashboard.Designer.cs
@@ -246,7 +246,6 @@
             // 
             // panelPanelZaGV
             // 
-            this.panelPanelZaGV.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelPanelZaGV.BackgroundImage")));
             this.panelPanelZaGV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panelPanelZaGV.Controls.Add(this.proba);
             this.panelPanelZaGV.GradientBottomLeft = System.Drawing.Color.White;
